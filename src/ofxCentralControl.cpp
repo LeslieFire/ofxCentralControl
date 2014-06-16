bca0807a015a373861615d26ff84f6296bd82709
Yang Fei <249893977@qq.com>
2014-06-16 23:23:32 +0800

    Update ofxCentralControl.cpp

--- src/ofxCentralControl.cpp
+++ src/ofxCentralControl.cpp
@@ -1,3 +1,35 @@
+/*
+ *  ofxCentralControl.cpp
+ *
+ *  Created by Leslie Yang on 06/16/14.
+ *  Copyright 2014 https://github.com/LeslieFire All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the author nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ *  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ *  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ *  OF THE POSSIBILITY OF SUCH DAMAGE.
+ *s
+ *  ************************************************************************************ */ 
+
+
 #include "ofxCentralControl.h"
 
 u8 BenQbaudRate = 0x04;
@@ -16,7 +48,7 @@ ofxCentralControl::ofxCentralControl(){
 	mProAddr = 0x01;			// projector address
 	mProCmd = 0x80;				// projector command
 	mProRet = 0x06;				// projector return command length
-	mFixedLen = 0x000A;			//¹Ì¶¨³¤¶È
+	mFixedLen = 0x000A;			//ï¿½Ì¶ï¿½ï¿½ï¿½ï¿½ï¿½
 	maxProNum = 16;
 
 	mPcHead = 0xCC;
